56cc164e08b9410863087a2d836d845f256b1cb1
Nasir <dev.nasiruddin@gmail.com>
2026-06-11 19:31:47 +0600

    feat: checkout

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const router = createBrowserRouter(
         <Route path="products" element={<Products />}></Route>
         <Route path="cart" element={<Cart />}></Route>
         <Route path="account" element={<Account />}></Route>
-        {/* <Route path="checkout" element={<Checkout />}></Route> */}
+        <Route path="checkout" element={<Checkout />}></Route>
         <Route path="productdetails/:id" element={<Details />}></Route>
         <Route path="wishlist" element={<Wishlist />}></Route>
         <Route path="*" element={<Error />}></Route>
